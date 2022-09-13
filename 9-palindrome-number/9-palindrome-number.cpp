d040555d38a9b336b53bddaef0570287ed589a98
MADARA587 <77529824+MADARA587@users.noreply.github.com>
2022-09-13 23:50:36 +0530

    Time: 15 ms (82.04%), Space: 6 MB (31.42%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long int temp = x;
+        long long int rev=0;
+        while(x>0)
+        {
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        if(rev==temp)
+          return true;
+        else
+        return false;
+    }
+};
